bc16d7626a5e335b77053246a12878149ee15d91
Aleksandr Fedorov <aleksandr.fedorov@onlyoffice.com>
2023-12-05 11:53:23 +0300

    delete: unused methods from Utils

--- onlyoffice-nuxeo-core/src/main/java/org/onlyoffice/utils/Utils.java
+++ onlyoffice-nuxeo-core/src/main/java/org/onlyoffice/utils/Utils.java
@@ -18,27 +18,12 @@
 
 package org.onlyoffice.utils;
 
-import org.json.JSONArray;
-import org.nuxeo.ecm.core.api.DocumentModel;
 import org.nuxeo.ecm.core.api.DocumentRef;
 import org.nuxeo.ecm.core.model.Document;
 import org.nuxeo.ecm.core.model.Session;
-import org.onlyoffice.model.Format;
-
-import java.util.List;
 
 public interface Utils {
-    public String getPathLocale(String language);
-    public String getDocumentKey(DocumentModel model);
-    public String getTitleWithoutExtension(String filename);
-    public String getFileExtension(String filename);
-    public String getDocumentType(String ext);
-    public Boolean isViewable(String extension);
-    public Boolean isEditable(String extension);
-    public Boolean isFillForm(String extension);
     public String getChangeToken(String key);
-    public String getMimeType(String extension);
     public Document resolveReference(Session session, DocumentRef docRef);
-    public List<Format> getSupportedFormats();
-    public JSONArray getSupportedFormatsAsJson();
+    String getMimeType(String extension);
 }
